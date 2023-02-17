5713b42bafacfc86e6133a0b27bcc75ee653052d
matheusvsm <matheusmoura.eng.eletrica@gmail.com>
2023-02-17 01:14:05 -0300

    Correção front

--- app.py
+++ app.py
@@ -4,14 +4,28 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://5nsGyX2gWPUACLso:5nsGyX2gWPUACLso@api.6zng7jt.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://5nsGyX2gWPUACLso:5nsGyX2gWPUACLso@api.6zng7jt.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client["mydb"]
 products_collection = db["products"]
 
+
 @app.route("/products", methods=["GET"])
 def get_products():
-    products = list(products_collection.find({}))
-    return jsonify(products)
+    page = int(request.args.get('page', 1))
+    limit = int(request.args.get('limit', 10))
+    query = request.args.get('query', {})
+
+    try:
+        products = list(products_collection.find(query).skip((page - 1) * limit).limit(limit))
+
+        if len(products) == 0:
+            return jsonify({"message": "No products found"}), 404
+        else:
+            return jsonify(products), 200
+
+    except:
+        return jsonify({"message": "Error processing request"}), 500
+
 
 @app.route("/products", methods=["POST"])
 def create_product():
@@ -21,31 +35,46 @@ def create_product():
     name = request.json["name"]
     price = request.json["price"]
     product = {"name": name, "price": price}
+
     result = products_collection.insert_one(product)
     product["_id"] = str(result.inserted_id)
+
     return jsonify(product)
 
-@app.route("/products/<id>", methods=["PUT"])
-def update_product(id):
-    if not request.json or not 'name' in request.json or not 'price' in request.json:
-        return jsonify({'error': 'The request must have the name and price fields'}), 400
 
-    name = request.json["name"]
-    price = request.json["price"]
+@app.route("/products/<id>", methods=["PATCH"])
+def update_product(id):
+    name = request.json.get("name")
+    price = request.json.get("price")
+    if not name and not price:
+        return jsonify({'error': 'The request must have the name or price fields'}), 400
+
     product = products_collection.find_one({"_id": ObjectId(id)})
     if not product:
         return jsonify({'error': 'Product not found'}), 404
-    products_collection.update_one({"_id": ObjectId(id)}, {"$set": {"name": name, "price": price}})
+
+    update_data = {}
+    if name:
+        update_data["name"] = name
+    if price:
+        update_data["price"] = price
+
+    products_collection.update_one({"_id": ObjectId(id)}, {"$set": update_data})
     product = products_collection.find_one({"_id": ObjectId(id)})
     product["_id"] = str(product["_id"])
+
     return jsonify(product)
+
 
 @app.route("/products/<id>", methods=["DELETE"])
 def delete_product(id):
     result = products_collection.delete_one({"_id": ObjectId(id)})
+
     if result.deleted_count == 0:
         return jsonify({'error': 'Product not found'}), 404
-    return jsonify({"message": "Product deleted"})
+    else:
+        return jsonify({"message": "Product deleted"})
+
 
 if __name__ == "__main__":
     app.run()
