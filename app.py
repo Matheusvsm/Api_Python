9d9b0d4b23b86f50f05c9a07461b05c7339a21b3
matheusvsm <matheusmoura.eng.eletrica@gmail.com>
2023-02-18 22:00:51 -0300

    Tentando concertar o frontend, utilizando html e js

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from products import products
 
 
 app = Flask(__name__)
 app.register_blueprint(products)
+
+@app.route('/')
+def index():
+    return render_template('./templates/index.html')
+
 
 if __name__ == "__main__":
     app.run()
---
+++ templates/static/app.js
@@ -0,0 +1,53 @@
+// carrega todos os produtos existentes na tabela
+function loadProducts() {
+    $.ajax({
+        url: "http://127.0.0.1:5000/products",
+        type: "GET",
+        success: function(data) {
+            var productsTable = $("#products-table tbody");
+            productsTable.empty();
+            for (var i = 0; i < data.length; i++) {
+                var product = data[i];
+                productsTable.append("<tr data-id='" + product.id + "'>" +
+                    "<td>" + product.id + "</td>" +
+                    "<td>" + product.name + "</td>" +
+                    "<td>" + product.price + "</td>" +
+                    "<td><button class='edit-product-button'>Editar</button> " +
+                    "<button class='delete-product-button'>Excluir</button></td>" +
+                    "</tr>");
+            }
+        }
+    });
+}
+
+// adiciona um novo produto ao banco de dados através da API
+function addProduct(name, price) {
+    $.ajax({
+        url: "http://127.0.0.1:5000/products",
+        type: "POST",
+        data: { "name": name, "price": price },
+        success: function() {
+            loadProducts();
+        }
+    });
+}
+
+// atualiza um produto existente no banco de dados através da API
+function updateProduct(id, name, price) {
+    $.ajax({
+        url: "http://127.0.0.1:5000/products/" + id,
+        method: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify({
+            name: name,
+            price: price
+        }),
+        success: function(result) {
+            alert("Produto atualizado com sucesso!");
+            location.reload();
+        },
+        error: function(xhr, resp, text) {
+            console.log(xhr, resp, text);
+        }
+    });
+}
